c4f2cae102fe41ef58b2ecd4c94058ff9c0077ef
joseAmzz <joseachemazzei@gmail.com>
2024-05-28 11:41:57 -0300

    First Commit

---
+++ src/test/java/com/example/Literarura/LiteraruraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Literarura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraruraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
